90480ffa032515f81f99c04eae1b394d2d56c156
DanRG1 <danielrochagal@gmail.com>
2025-08-06 14:22:18 -0300

    feat(frontend): criar componente TestDepartments para listar departamentos via API

---
+++ frontend/src/components/TestDepartments.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Departments</h2>
+    <ul>
+      <li v-for="department in departments" :key="department.id">{{ department.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import api from '../services/api';
+
+export default {
+  name: 'TestDepartments',
+  data() {
+    return {
+      departments: [],
+    };
+  },
+  mounted() {
+    api.get('/departments')
+      .then(response => {
+        this.departments = response.data;
+      })
+      .catch(error => {
+        console.error('Error fetching departments:', error);
+      });
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  color: green;
+}
+</style>
