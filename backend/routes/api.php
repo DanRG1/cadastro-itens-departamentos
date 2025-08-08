08c9b540fe821f72476653c0ac3f592f44e2ac48
DanRG1 <danielrochagal@gmail.com>
2025-08-08 19:30:17 -0300

    ajustes nas rotas da api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\DepartamentoController;
 
-Route::apiResource('items', ItemController::class);
-Route::apiResource('departments', DepartmentController::class);
+Route::apiResource('departments', DepartamentoController::class);
 
 
 
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\ItemsController;
+
+Route::apiResource('departments', DepartamentoController::class);
+Route::apiResource('items', ItemsController::class);
 
 Route::get('/', function () {
     return view('welcome');
