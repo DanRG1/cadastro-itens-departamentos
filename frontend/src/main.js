094354802bdfbdf2a31a416a1522479703ba9bf6
DanRG1 <danielrochagal@gmail.com>
2025-08-08 16:27:20 -0300

    instalação do vue com vite

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,5 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
