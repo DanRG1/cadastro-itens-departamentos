4825ecfdadfd8a1b0e9c9f4ac26e57980cc7935f
DanRG1 <danielrochagal@gmail.com>
2025-08-08 19:29:55 -0300

    create model items\

---
+++ backend/app/Models/Items.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Items extends Model
+{
+    protected $table = 'items';
+
+    protected $fillable = ['name', 'quantity', 'description', 'department_id'];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'department_id');
+    }
+}
